514acf6b0457fe4216fb367efb801f4d598c653d
Prashant C Chaturvedi <pccfrmvns@gmail.com>
2015-12-13 15:39:29 +0530

    cooke based session management and remember me option

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151212094058) do
+ActiveRecord::Schema.define(version: 20151213092347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20151212094058) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
