408bebcbaca499739fb3c6b9c620c85b994310de
Prashant C Chaturvedi <pccfrmvns@gmail.com>
2016-01-19 23:57:46 +0530

    updated nabber and footer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,22 +3,6 @@ class PagesController < ApplicationController
   def home
   end
   
-  def work
-  end
   
-  def people
-  end
-
-  def blog
-  end
-
-  def privacy
-  end
-
-  def contact
-  end
-
-  def about
-  end
 
 end
